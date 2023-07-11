524f9bd26e9e12529f23e5c2b8e17b11b36ccf19
Andrew Arnott <andrewarnott@live.com>
2023-07-10 20:53:24 -0600

    Add support for webassembly

--- src/nerdbank-zcash-rust/build_all.ps1
+++ src/nerdbank-zcash-rust/build_all.ps1
@@ -22,7 +22,16 @@ Push-Location $PSScriptRoot
 $rustTargets = @(..\..\azure-pipelines\Get-RustTargets.ps1)
 $rustTargets | % { $buildArgs += "--target=$_" }
 
+if ($env:BUILD_BUILDID) {
+    Write-Host "##[command]cargo build @buildArgs"
+}
 cargo build @buildArgs
+
+# Special handling for building the wasm32-unknown-unknown target as it requires the nightly build of rust.
+if ($env:BUILD_BUILDID) {
+    Write-Host "##[command]cargo +nightly build -Zbuild-std --target=wasm32-unknown-unknown"
+}
+cargo +nightly build -Zbuild-std --target=wasm32-unknown-unknown
 
 $configPathSegment = 'debug'
 if ($Release) { $configPathSegment = 'release' }
